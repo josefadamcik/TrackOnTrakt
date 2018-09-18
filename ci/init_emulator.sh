faa0122651b3d8dd2726a4a0a29a5672544033f9
Josef Adamcik <josef.adamcik@gmail.com>
2018-09-18 12:03:13 +0200

    Build tuning.

--- ci/init_emulator.sh
+++ ci/init_emulator.sh
@@ -2,5 +2,6 @@
 adb shell settings put global window_animation_scale 0 &
 adb shell settings put global transition_animation_scale 0 &
 adb shell settings put global animator_duration_scale 0 &
-#adb shell pm grant cz.josefadamcik.trackontrakt.internal.dev android.permission.WRITE_EXTERNAL_STORAGE &
+adb shell pm grant cz.josefadamcik.trackontrakt.dev android.permission.READ_EXTERNAL_STORAGE
+adb shell pm grant cz.josefadamcik.trackontrakt.dev android.permission.WRITE_EXTERNAL_STORAGE
 adb shell input keyevent 82 &
